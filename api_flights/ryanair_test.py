2f8879ceed4fc3c388ae78a9b3fbcf345966c308
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2023-06-30 15:21:02 +0100

    utilities

--- api_flights/ryanair_test.py
+++ api_flights/ryanair_test.py
@@ -3,9 +3,16 @@ from datetime import datetime, timedelta
 
 api = Ryanair(currency="GBP")
 day1 = datetime.today().date()
-day2 = day1 + timedelta(days=1)
+day2 = day1 + timedelta(days=3)
 
-flights = api.get_cheapest_flights("STN", day1, day1)
+flights = api.get_cheapest_return_flights(source_airport = "STN",
+                                    date_from = day1,
+                                    date_to = day1,
+                                    return_date_from = day2,
+                                    return_date_to = day2)
 
-f = flights[0]
-print(f)
+f = flights
+# f.sort(key = lambda x : x.totalPrice)
+
+for x in f[:10]:
+    print(str(x) + '\n')
