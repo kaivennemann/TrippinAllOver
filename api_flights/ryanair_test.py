e5704fd42417d630a59b2cf3bddc5726c55151a5
Thomasbeso <esothomas@gmail.com>
2023-06-30 15:36:35 +0100

    added print_flights

--- api_flights/ryanair_test.py
+++ api_flights/ryanair_test.py
@@ -1,19 +1,17 @@
 from ryanair import Ryanair
 from datetime import datetime, timedelta
-from ryanair_utilities import print_flight
+from ryanair_utilities import print_flights
 
 api = Ryanair(currency="GBP")
 day1 = datetime.today().date()
 day2 = day1 + timedelta(days=3)
 
-flights = api.get_cheapest_return_flights(source_airport = "STN",
-                                    date_from = day1,
-                                    date_to = day1,
-                                    return_date_from = day2,
-                                    return_date_to = day2)
-
+flights = api.get_cheapest_return_flights(source_airport="STN",
+                                          date_from=day1,
+                                          date_to=day1,
+                                          return_date_from=day2,
+                                          return_date_to=day2)
 
 # flights.sort(key = lambda x : x.totalPrice)
-
-for f in flights[:5]:
-    print_flight(f)
+f = flights[0]
+print_flights(flights)
--- api_flights/ryanair_utilities.py
+++ api_flights/ryanair_utilities.py
@@ -1,3 +1,12 @@
-def print_flight(flight):
-    s = str(flight) + '\n'
-    print(s)
+import ryanair.types
+from tabulate import tabulate
+
+def print_flights(flights: list[ryanair.types.Trip]):
+    table = []
+
+    for flight in flights:
+        dest = str(flight.outbound.destinationFull) + " (" + str(flight.outbound.destination) + ")"
+        price = str(round(float(flight.totalPrice), 2))
+        table.append(["Dest:", dest, "Price:", "£" + price])
+
+    print(tabulate(table))
