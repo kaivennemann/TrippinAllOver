6772cdd0fa82f3b40bd693d2256878c6bbf762ac
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2023-08-22 21:57:13 -0700

    ideas

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+"""
+Ideas:
+    - add in a europe booking agency API or something to book hostels
+    - figure out how payment card APIs work
+    - package everything up as a weekend trip for Cambridge students (flights + hotel + maybe smth extra)
+
+"""
+
 
 # from api_flights.flight_api import FlightAPI
 from utilities.event_calendar import EventCalendar, DateRange, TripLength
@@ -8,6 +16,7 @@ from datetime import date
 
 api = FlightAPI()
 
+
 # Method 1
 # flights = api.get_cheapest_roundtrips(
 #     'STN',
@@ -16,10 +25,11 @@ api = FlightAPI()
 # )
 # print_trips(flights)
 
+
 # Method 2
 flights = api.get_cheapest_roundtrips(
     'STN',
-    EventCalendar.of([(date(2023,10,11), date(2023,10,13))]),
-    TripLength(3, 3)
+    EventCalendar.of([(date(2023,11,5), date(2023,11,8))]),
+    TripLength(2, 3)
 )
 print_trips(flights)
