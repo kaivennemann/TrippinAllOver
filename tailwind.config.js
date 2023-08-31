eb41b97ce40e479ebc5b5a63698328669da69e4e
Thomasbeso <esothomas@gmail.com>
2023-08-31 17:27:15 +0100

    added some dummy frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   content: ["./src/website/booking/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'navy-blue': '#100740',
+    },
+    borderRadius: {
+        DEFAULT: '4px',
+        'large': '12px',
+    }
   },
   plugins: [],
 }
